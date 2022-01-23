98024ee1008afd0bc5ea6d486b2d2fc057bd9afe
vladis_step <vlstep2200@gmail.com>
2022-01-23 14:30:58 +0300

    Local models: Привел к формату папку Cats_vs_dogs

--- Local_methods/Cats_vs_dogs/CVD_create_mode.py
+++ Local_methods/Cats_vs_dogs/CVD_create_mode.py
@@ -17,33 +17,7 @@ PATH_TO_DATA = my_utils.path_to_project + '/Datasets/dogs-vs-cats'
 CONTENT_DIR = PATH_TO_DATA + '/content'
 TRAIN_DIR = CONTENT_DIR + '/train'
 VALID_DIR = CONTENT_DIR + '/valid'
-PATH_TO_SAVE_MODEL = my_utils.path_to_project + "Lime/Cats_vs_dogs/Models/" + "my_cats_vs_dogs_model.h5"
-
-# for dirname, _, filenames in os.walk(path_to_data):
-#     for filename in filenames:
-#         print(os.path.join(dirname, filename))
-
-
-if not os.path.exists(CONTENT_DIR):
-    # Extract dataset
-    import zipfile
-    with zipfile.ZipFile(PATH_TO_DATA + '/train.zip', 'r') as zipf:
-        zipf.extractall(CONTENT_DIR)
-
-    # Split cats and dogs images to train and valid datasets
-    img_filenames = os.listdir(TRAIN_DIR)
-    dog_filenames = [fn for fn in img_filenames if fn.startswith('dog')]
-    cat_filenames = [fn for fn in img_filenames if fn.startswith('cat')]
-    dataset_filenames = train_test_split(
-        dog_filenames, cat_filenames, test_size=0.1, shuffle=True, random_state=42
-    )
-    # Move images
-    make_dirs = [d + a for a in ['/dog', '/cat'] for d in [TRAIN_DIR, VALID_DIR]]
-    for dir, fns in zip(make_dirs, dataset_filenames):
-        os.makedirs(dir, exist_ok=True)
-        for fn in tqdm.tqdm(fns):
-            shutil.move(os.path.join(TRAIN_DIR, fn), dir)
-        print('elements in {}: {}'.format(dir, len(os.listdir(dir))))
+PATH_TO_SAVE_MODEL = my_utils.path_to_project + "Local_methods/Cats_vs_dogs/Models/" + "my_cats_vs_dogs_model.h5"
 
 BATCH_SIZE = 32
 IMAGE_SHAPE = 128
