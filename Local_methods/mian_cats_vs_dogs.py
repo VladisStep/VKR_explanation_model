54b566f132dd90364437b2dcd92d0a9fecf75096
vladis_step <vlstep2200@gmail.com>
2022-01-24 16:32:10 +0300

    Local models: Поправил main_cats_vs_dogs.py и добавил графики

--- Local_methods/mian_cats_vs_dogs.py
+++ Local_methods/mian_cats_vs_dogs.py
@@ -6,27 +6,27 @@ from tensorflow import keras
 import os
 
 
-from Lime import MyAnchorExplainer, MyIntegratedGradient, MyLimeExplainer
+from Local_methods import MyAnchorExplainer, MyIntegratedGradient, MyLimeExplainer
 
 PATH_TO_DATASET = my_utils.path_to_project + '/Datasets/dogs-vs-cats'
 CONTENT_DIR = PATH_TO_DATASET + '/content'
 TRAIN_DIR = CONTENT_DIR + '/train'
 VALID_DIR = CONTENT_DIR + '/valid'
-PATH_TO_MODEL = my_utils.path_to_project + "Lime/Cats_vs_dogs/Models/" + "my_cats_vs_dogs_model.h5"
-PATH_TO_SAVE_GRAPHS = my_utils.path_to_project + "Lime/Cats_vs_dogs/Graphs/"
+PATH_TO_MODEL = my_utils.path_to_project + "Local_methods/Cats_vs_dogs/Models/" + "my_cats_vs_dogs_model.h5"
+PATH_TO_SAVE_GRAPHS = my_utils.path_to_project + "Local_methods/Cats_vs_dogs/Graphs/"
 IMAGE_SHAPE = 128
 
 
 model = keras.models.load_model(PATH_TO_MODEL)
 
 images = [
-    ["cat.3", "/cat/cat.3.jpg"],
-    ["cat.13", "/cat/cat.13.jpg"],
-    ["cat.16", "/cat/cat.38.jpg"],
+    ["CVD_cat.3", "/cat/cat.3.jpg"],
+    ["CVD_cat.13", "/cat/cat.13.jpg"],
+    ["CVD_cat.16", "/cat/cat.38.jpg"],
 
-    ["dog.0", "/dog/dog.0.jpg"],
-    ["dog.7", "/dog/dog.7.jpg"],
-    ["dog.31", "/dog/dog.31.jpg"],
+    ["CVD_dog.0", "/dog/dog.0.jpg"],
+    ["CVD_dog.7", "/dog/dog.7.jpg"],
+    ["CVD_dog.31", "/dog/dog.31.jpg"],
 
     # ["tr-cat.0", "/cat/cat.0.jpg"],
     # ["tr-cat.1", "/cat/cat.1.jpg"],
@@ -41,16 +41,16 @@ images = [
 
 # MyLimeExplainer.explanation(model, image.img_to_array(load_img(path=VALID_DIR + images[0][1],
 #                             target_size=(IMAGE_SHAPE, IMAGE_SHAPE))).astype("double") / 255.0,
-#                             path_to_save=my_utils.path_to_project +"Lime/Cats_vs_dogs/Graphs/",
+#                             path_to_save=my_utils.path_to_project +"Local_methods/Cats_vs_dogs/Graphs/",
 #                             image_name='test')
 # MyAnchorExplainer.explanation(model, image.img_to_array(load_img(path=VALID_DIR + images[0][1],
 #                             target_size=(IMAGE_SHAPE, IMAGE_SHAPE))).astype("double") / 255.0,
-#                             path_to_save=my_utils.path_to_project +"Lime/Cats_vs_dogs/Graphs/",
+#                             path_to_save=my_utils.path_to_project +"Local_methods/Cats_vs_dogs/Graphs/",
 #                             image_name='test')
 # MyIntegratedGradient.explanation(model, image.img_to_array(load_img(
 #                             path=VALID_DIR + images[0][1],
 #                             target_size=(IMAGE_SHAPE, IMAGE_SHAPE))).astype("double") / 255.0,
-#                             path_to_save=my_utils.path_to_project +"Lime/Cats_vs_dogs/Graphs/",
+#                             path_to_save=my_utils.path_to_project +"Local_methods/Cats_vs_dogs/Graphs/",
 #                             image_name='test')
 
 
@@ -87,27 +87,19 @@ def getExplanations(pathList, name):
 
         loaded_img = processImgToModel(pathList[i])
 
-        # MyLimeExplainer.explanation(model, loaded_img, num_samples=500,
-        #                             image_name=name + str(i),
-        #                             path_to_save=PATH_TO_SAVE_GRAPHS)
-        MyAnchorExplainer.explanation(model, loaded_img,
+        MyLimeExplainer.explanation(model, loaded_img, num_samples=500,
                                     image_name=name + str(i),
                                     path_to_save=PATH_TO_SAVE_GRAPHS)
         MyAnchorExplainer.explanation(model, loaded_img,
-                                      threshold=0.8,
+                                    image_name=name + str(i),
+                                    path_to_save=PATH_TO_SAVE_GRAPHS)
+        MyIntegratedGradient.explanation(model, loaded_img,
                                       image_name=name + str(i),
                                       path_to_save=PATH_TO_SAVE_GRAPHS)
-        MyAnchorExplainer.explanation(model, loaded_img,
-                                      threshold=0.7,
-                                      image_name=name + str(i),
-                                      path_to_save=PATH_TO_SAVE_GRAPHS)
-        # MyIntegratedGradient.explanation(model, loaded_img,
-        #                               image_name=name + str(i),
-        #                               path_to_save=PATH_TO_SAVE_GRAPHS)
 
 
 def anchorTest(pathList, name):
-    PATH_TO_ANCHOR_TESTS = my_utils.path_to_project + "Lime/Cats_vs_dogs/AnchorTests/"
+    PATH_TO_ANCHOR_TESTS = my_utils.path_to_project + "Local_methods/Cats_vs_dogs/AnchorTests/"
     for i in range(len(pathList)):
         loaded_img = processImgToModel(pathList[i])
         MyAnchorExplainer.explanation(model, loaded_img,
